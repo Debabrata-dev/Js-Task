8aaafebdbea651887ee7526b22661558d38b3e6c
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-24 14:51:24 +0530

    Add files via upload

---
+++ Revies.js
@@ -0,0 +1,49 @@
+// let obj={
+//     'full name':"Virat khoil",
+//      salry:40000000,
+//      age:36,
+// }
+// console.log(obj);
+
+// let str="Hello You Have Forget your drem";
+// let word=str.split();
+// console.log(word);
+// console.log(word.join());
+
+// let str="Debarata Das";
+// console.log(str.substring(2));
+
+function getname(){
+    console.log("MSD");
+}
+getname();
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(9,9));
+
+//Arrow Function
+
+let getSequar=(a,b)=>{
+ let ans=a**b;
+ return ans;
+}
+console.log(getSequar(9,2))
+
+let arr=[12,21,23,31,32,33,41];
+arr.push(11);
+console.log(arr);
+arr.pop(11);
+console.log(arr);
+
+arr.shift();
+console.log(arr)
+arr.shift();
+console.log(arr)
+
+arr.unshift(90);//add element in left side
+console.log(arr);
+
+let arr1=[12,11,3,4,5,14,15]
+
