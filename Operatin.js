3a280f7f63860ac2981af3cca5ebdc10606acb1c
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-28 22:49:30 +0530

    Add files via upload

---
+++ Operatin.js
@@ -0,0 +1,16 @@
+console.log("babbar");
+//Math object
+console.log(Math.PI);
+console.log(Math.max(90,60,50,70,600,712,89));
+console.log(Math.min(90,60,50,70,600,712,89));
+console.log(Math.round(1.4));//Round figure
+console.log(Math.round(2.9));//Round figure
+console.log(Math.floor(1.2));//just smallest integer value
+console.log(Math.ceil(1.1));//just Big number
+console.log(Math.abs(-5));//absolut value
+
+console.log(Math.random());//Random number between 0 to 1.
+
+console.log(Math.sqrt(2));//It gives use to root over value or square root value
+
+console.log(Math.pow(9,2));//It gives use to power of a number
