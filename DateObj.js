ed91165750bc178b4a964dad1312042573c716db
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-28 23:37:10 +0530

    Add files via upload

---
+++ DateObj.js
@@ -0,0 +1,8 @@
+let curr=new Date();
+let date=new Date('June 20 1998 07:15');
+let newDate=new Date(1998,6,20,7);
+console.log(curr)//Curent date time
+console.log(date);
+console.log(newDate.getFullYear())
+console.log(newDate.getMonth())
+console.log(newDate.getTime())
