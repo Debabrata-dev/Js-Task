b0a4c5bfe0925e0c29b32204c0ee3a65701be2dc
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-29 19:52:19 +0530

    Add files via upload

---
+++ Errorhandel.js
@@ -0,0 +1,20 @@
+// try{
+// console.log(x);
+// }
+// catch(e){
+// console.log("404 ERROR");
+// }
+
+
+// try{
+//     let x=96;
+//     console.log(x);
+// }
+// catch(err){
+//     throw new Error("Bhai pehle Declar karo ");
+// }
+
+let errCode=100;
+if(errCode==100){
+    throw new Error("Invalid Json");
+}
