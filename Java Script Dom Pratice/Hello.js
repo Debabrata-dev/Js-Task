594f6f7fd1ba3e3673cf88d477f5d076202e08b3
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-12-16 23:13:18 +0530

    Add files via upload

---
+++ Java Script Dom Pratice/Hello.js
@@ -0,0 +1,7 @@
+const ele=document.getElementsByClassName("example");
+ele[0].style.backgroundColor="red";
+ele[0].style.color="white";
+// ele[1].style.backgroundColor="red";
+// ele[2].style.backgroundColor="red";
+// ele[3].style.backgroundColor="red";
+const ele1=document.get
